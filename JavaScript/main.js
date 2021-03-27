c34c8248a816ccb9a66a732fc9b115f72b7d1611
Konstantinos Krystallis <konkry1@hotmail.com>
2021-03-27 16:47:47 +0200

    time.js added & fixed js path in home

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,14 +1,1 @@
-function startTime() {
-  var today = new Date();
-  var h = today.getHours();
-  var m = today.getMinutes();
-  var s = today.getSeconds();
-  m = checkTime(m);
-  s = checkTime(s);
-  document.getElementById('display-time').innerHTML = h + ":" + m + ":" + s;
-  var t = setTimeout(startTime, 500);
-}
-function checkTime(i) {
-  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
-  return i;
-}
+//TODO
